00c76919982db2443ecf39194e89ed2ce39c0150
Robert Wild <wildr2@rpi.edu>
2017-03-22 20:09:34 -0400

    Move timer to toolbar

--- Aisle4Android/app/src/main/java/sdd/aisle4android/ListActivity.java
+++ Aisle4Android/app/src/main/java/sdd/aisle4android/ListActivity.java
@@ -4,14 +4,18 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v4.app.DialogFragment;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.view.menu.MenuView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -23,15 +27,15 @@ public class ListActivity extends AppCompatActivity implements AddItemDialog.Lis
     private ShopList shopList;
 
     private Toolbar toolbar;
+    private MenuItem timerItem;
 
     private ArrayAdapter<String> arrayAdapter;
 
     private Handler timerHandler;
     private Runnable timerUpdater = new Runnable() {
-        @Override
-        public void run() {
+        @Override public void run() {
             String timeStr = secondsToString((int)(app.getShoppingTime() / 1000));
-            toolbar.setTitle(shopList.getName() + "  " + timeStr);
+            timerItem.setTitle(timeStr);
             timerHandler.postDelayed(this, 1000);
         }
     };
@@ -61,7 +65,12 @@ public class ListActivity extends AppCompatActivity implements AddItemDialog.Lis
         }
     }
     @Override public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.toolbar, menu);
+        getMenuInflater().inflate(R.menu.list_toolbar, menu);
+
+        // Timer
+        timerItem = menu.findItem(R.id.list_timer);
+        timerHandler = new Handler();
+
         return true;
     }
     @Override public void onAddItemDialogConfirm(String itemName) {
@@ -102,9 +111,6 @@ public class ListActivity extends AppCompatActivity implements AddItemDialog.Lis
         arrayAdapter = new ArrayAdapter<String>(
                 this, android.R.layout.simple_list_item_1, shopItemNames);
         list.setAdapter(arrayAdapter);
-
-        // Timer
-        timerHandler = new Handler();
     }
 
     private String secondsToString(int pTime) {
